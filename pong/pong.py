28a2a6652d41b744bb98a8fe070a1e7cb341292e
--global <--global>
2023-06-20 19:30:27 +0500

    Scoring corrected, rest left

--- pong/pong.py
+++ pong/pong.py
@@ -9,7 +9,7 @@ class PongBoard():
         self.moving_up = False
         self.moving_down = False
     def draw(self):
-        pygame.draw.rect(screen, (255, 255, 255), self.rect, width=3)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect)
     def update(self):
         if self.moving_up:
             self.rect.y -= self.velocity
@@ -42,14 +42,9 @@ def check_collisions(pb_1, pb_2, border_1, border_2, ball):
     # The parameter of reflect is the normal vector for reflection
     br = ball.rect
     pb = pb_1
-    list_1 = [(x, pb.rect.bottom - 1) for x in range(100, 120)]
-    list_2 = [(119, y) for y in range(pb.rect.top, pb.rect.bottom)]
-    list_3 = [(x, pb.rect.top + 1) for x in range(100, 120)]
     if pygame.Rect.colliderect(pb_1.rect, br):
         ball.direction = ball.direction.reflect((1, 0))
-        # Do the conditionals with the lists
     if pygame.Rect.colliderect(pb_2.rect, br):
-        pb = pb_2
         ball.direction = ball.direction.reflect((1, 0))
     if pygame.Rect.colliderect(border_1, br) or pygame.Rect.colliderect(border_2, br):
         ball.direction = ball.direction.reflect((0, 1))
@@ -57,6 +52,27 @@ def check_collisions(pb_1, pb_2, border_1, border_2, ball):
             ball.rect.y = border_1.bottom
         elif ball.rect.y > border_2.top and not 1000 <= ball.rect.x <= 0:
             ball.rect.y = border_2.top
+
+def color_animation(r, g, b, r_change, g_change, b_change):
+    screen.fill((r, g, b))
+    r += r_change
+    if r == 255:
+        r_change *= -1
+        g += g_change
+    if g == 255:
+        g_change *= -1
+        b += b_change
+    if b == 255:
+        b_change *= -1
+    if r == 0:
+        r_change *= -1
+    if g == 0:
+        g_change *= -1
+        g += g_change
+    if b == 0:
+        b_change *= -1
+        b += b_change
+    return r, g, b, r_change, g_change, b_change
 
 # Basic setup
 pygame.init()
@@ -70,6 +86,8 @@ pb_2 = PongBoard((900, 495, 20, 80))
 border_1 = pygame.Rect(0, 5, 1000, 10)
 border_2 = pygame.Rect(0, 585, 1000, 10)
 ball = Ball(-1)
+r, g, b = 1, 1, 1
+r_change, g_change, b_change = 1, 1, 1
 
 # For scores
 score_1 = 0
@@ -83,9 +101,9 @@ def display_score(score, dimensions):
     screen.blit(font_surface, font_rect)
 
 while True:
-    # animate the bg_color later
-    screen.fill((00, 00, 12))
-    
+    # animate the bg_color
+    color_animation(r,g,b,r_change,g_change,b_change)
+
     # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -111,9 +129,9 @@ while True:
  
     # For displaying the scores
     if ball.rect.right <= 0:
-        score_1 += 1
-    elif ball.rect.left >= 1000:
         score_2 += 1
+    elif ball.rect.left >= 1000:
+        score_1 += 1
 
     display_score(str(score_1), (450, 50, 30, 50))
     display_score(str(score_2), (525, 50, 30, 50))
